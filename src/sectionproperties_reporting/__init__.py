07b5249c851caec26bb679bc5c706b3f9e72e2b5
Connor Ferster <connorferster@gmail.com>
2025-06-29 07:25:01 -0700

    initial commit

---
+++ src/sectionproperties_reporting/__init__.py
@@ -0,0 +1,12 @@
+"""
+sectionproperties_reporting is a package of convenience functions
+to reduce the work of extracting analysis results from sectionproperties.
+
+Includes functions for extracting both stress results and properties results.
+
+- Connor Ferster, Structural Python, 2025
+"""
+
+__version__ = "0.1.0"
+
+from .extraction import *
